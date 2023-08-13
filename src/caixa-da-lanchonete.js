2bd8baeb9f2b714c597ecd87740b46fc573bfd58
João Dias <joao.dias@orpen.com.br>
2023-08-13 17:46:10 -0300

    docs: Adiciona informações sobre o cardápio da lanchonete

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,6 +1,18 @@
 class CaixaDaLanchonete {
 
     /**
+     *  Código      Descrição                   Valor
+     *  cafe        Café                        R$ 3,00
+     *  chantily    Chantily (extra do Café)    R$ 1,50
+     *  suco        Suco Natural                R$ 6,20
+     *  sanduiche   Sanduíche                   R$ 6,50
+     *  queijo      Queijo (extra do Sanduíche) R$ 2,00
+     *  salgado     Salgado                     R$ 7,25
+     *  combo1      1 Suco e 1 Sanduíche        R$ 9,50
+     *  combo2      1 Café e 1 Sanduíche        R$ 7,50
+     */
+
+    /**
      *  Descontos e taxas:
      *  Pagamento em dinheiro tem 5% de desconto;
      *  Pagamento a crédito tem acréscimo de 3% no valor total.
