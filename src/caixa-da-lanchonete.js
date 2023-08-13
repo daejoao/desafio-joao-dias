6055f596118d3f2d505d835e8b4054c067cccd27
João Dias <joao.dias@orpen.com.br>
2023-08-13 16:37:42 -0300

    docs: Adiciona comentários sobre os parâmetros do método calcularValorDaCompra

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,6 +1,9 @@
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
+        // metodoDePagamento: string | possíveis valores: 'debito', 'credito', 'dinheiro';
+        // itens: array | itens que serão comprados. Cada item é uma string contendo o código do item e a quantidade separados por uma vírgula. Ex: ['cafe,1','chantily,1'];
+
         return "";
     }
 
